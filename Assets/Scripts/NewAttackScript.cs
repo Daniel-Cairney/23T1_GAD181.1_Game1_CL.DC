e53fbd6f8f4094b624d9bec3a9193cbcee98ccf5
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2023-02-19 14:09:58 +1100

    fixed a small bug
    the DamageCalc and AttackScript need to be on the same object to work

--- Assets/Scripts/NewAttackScript.cs
+++ Assets/Scripts/NewAttackScript.cs
@@ -5,64 +5,70 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NewAttackScript : MonoBehaviour
+namespace DanCon
 {
-    public Text parryResult;
-
-    [SerializeField] private GameObject playerOne;
-    [SerializeField] private GameObject playerTwo;
 
 
-    void Update()
+
+    public class NewAttackScript : MonoBehaviour
     {
-        if(Input.GetKey(KeyCode.A))
+        public Text parryResult;
+
+        [SerializeField] private GameObject playerOne;
+        [SerializeField] private GameObject playerTwo;
+
+
+        void Update()
         {
-            if(Input.GetKey(KeyCode.LeftArrow))
+            if (Input.GetKey(KeyCode.A))
             {
-                parryResult.text = "Parry";
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    parryResult.text = "Parry";
+                }
+                else if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    GetComponent<DamageCalculator>().HurtPlayerTwo();
+                }
+                else if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    GetComponent<DamageCalculator>().HurtPlayerOne();
+                }
             }
-            else if (Input.GetKey(KeyCode.DownArrow))
+
+            if (Input.GetKey(KeyCode.S))
             {
-                GetComponent<DamageCalculator>().HurtPlayerTwo();
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    GetComponent<DamageCalculator>().HurtPlayerOne();
+                }
+                else if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    parryResult.text = "Parry";
+                }
+                else if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    GetComponent<DamageCalculator>().HurtPlayerTwo();
+                }
             }
-            else if (Input.GetKey(KeyCode.RightArrow))
+
+            if (Input.GetKey(KeyCode.D))
             {
-                GetComponent<DamageCalculator>().HurtPlayerOne();
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    GetComponent<DamageCalculator>().HurtPlayerTwo();
+                }
+                else if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    GetComponent<DamageCalculator>().HurtPlayerOne();
+                }
+                else if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    parryResult.text = "Parry";
+                }
             }
+
+
         }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                GetComponent<DamageCalculator>().HurtPlayerOne();
-            }
-            else if (Input.GetKey(KeyCode.DownArrow))
-            {
-                parryResult.text = "Parry";
-            }
-            else if (Input.GetKey(KeyCode.RightArrow))
-            {
-                GetComponent<DamageCalculator>().HurtPlayerTwo();
-            }
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                GetComponent<DamageCalculator>().HurtPlayerTwo();
-            }
-            else if (Input.GetKey(KeyCode.DownArrow))
-            {
-                GetComponent<DamageCalculator>().HurtPlayerOne();
-            }
-            else if (Input.GetKey(KeyCode.RightArrow))
-            {
-                parryResult.text = "Parry";
-            }
-        }
-
-
     }
 }
