cddefaee3aec6e196a2d0560e825fa037a4ba800
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2023-02-27 20:16:27 +1100

    changing the movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,112 +10,20 @@ namespace DanCon
     public class PlayerMovement : MonoBehaviour
     {
 
-        public Sprite SamuraiPlaceholder_0, SamuraiPlaceholder_1, SamuraiPlaceholder_2, SamuraiPlaceholder_3;
+        [SerializeField] private float movementSpeed = 5f;
 
+        [SerializeField] private Animator samuraiAnim;
 
-        [SerializeField] private float movementSpeed;
-
-        private Rigidbody2D theRB2D;
-     
-        // these allow us to decide which keys allow for movement in the inspector
-
-        [SerializeField] private KeyCode stepRight;
-        [SerializeField] private KeyCode stepLeft;
-        [SerializeField] private KeyCode upAttack;
-        [SerializeField] private KeyCode midAttack;
-        [SerializeField] private KeyCode downAttack;
-
-        private void Start()
-        {
-            theRB2D = GetComponent<Rigidbody2D>();
-        }
-
-        // allows player to move forward, if they hold down the key
-        // if they let go of the key, they move back
-        // player tag MAY not be necesary here, but might be relevant if we make changes in the future
         private void Update()
-         {
-                        
-            if (gameObject.CompareTag("PlayerOne"))
-            {
-                
-                if (Input.GetKeyDown(stepRight))
-                {
-                    theRB2D.velocity = new Vector2(movementSpeed, theRB2D.velocity.y);
-                }
-                else if (Input.GetKeyUp(stepRight))
-                {
-                    theRB2D.velocity = new Vector2(-movementSpeed, theRB2D.velocity.y);
-                }
-            }
-
-            if (gameObject.CompareTag("PlayerOne"))
-
-            {
-
-
-                    if (Input.GetKeyDown(upAttack))
-                    {   // animation plays when A is pressed - but won't play again
-                        GetComponent<SpriteRenderer>().sprite = SamuraiPlaceholder_3;
-                    }
-
-                    else if (Input.GetKeyDown(midAttack))
-                    {
-                        GetComponent<SpriteRenderer>().sprite = SamuraiPlaceholder_2;
-                    }
-
-                    else if (Input.GetKeyDown(downAttack))
-                    {
-                        GetComponent<SpriteRenderer>().sprite = SamuraiPlaceholder_1;
-                    }
-
-                    //timeBtwAtk = startTimeBtwAtk;
-
-            }
-
-            // controls for player 2
-
-            if (gameObject.CompareTag("PlayerTwo"))
-            {
-                if (Input.GetKeyDown(stepLeft))
-                {
-                    theRB2D.velocity = new Vector2(-movementSpeed, theRB2D.velocity.y);
-                }
-
-                else if (Input.GetKeyUp(stepLeft))
-                {
-                    theRB2D.velocity = new Vector2(movementSpeed, theRB2D.velocity.y);
-                }
-
-            }
-
-            if (gameObject.CompareTag("PlayerTwo"))
-
-            {
-
-
-                    if (Input.GetKeyDown(upAttack))
-                    {   // animation plays when A is pressed - but won't play again
-                        GetComponent<SpriteRenderer>().sprite = SamuraiPlaceholder_3;
-                    }
-
-                    else if (Input.GetKeyDown(midAttack))
-                    {
-                        GetComponent<SpriteRenderer>().sprite = SamuraiPlaceholder_2;
-                    }
-
-                    else if (Input.GetKeyDown(downAttack))
-                    {
-                        GetComponent<SpriteRenderer>().sprite = SamuraiPlaceholder_1;
-                    }
-
-                    //timeBtwAtk = startTimeBtwAtk;
-
-               
-
-            }
+        {
+            
         }
 
- 
+        private void FixedUpdate()
+        {
+            
+        }
+
+
     }
 }
