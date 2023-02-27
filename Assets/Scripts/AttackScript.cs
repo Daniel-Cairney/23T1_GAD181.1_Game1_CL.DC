dc4d0081d785e312e2d987ebc68f8fb47564ded4
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2023-02-27 20:53:18 +1100

    adding the animations for each attack
    I've re-written the movement script
    and added animations to play with each attack

--- Assets/Scripts/AttackScript.cs
+++ Assets/Scripts/AttackScript.cs
@@ -9,156 +9,40 @@ namespace DanCon
 {
     public class AttackScript : MonoBehaviour
     {
-        public Text attackResult;
 
-        public GameObject playerOne;
-        public GameObject playerTwo;
+        [SerializeField] private Animator animator;
 
-        
-        public string[] attackChoices ;
-        public string[] playerChoices ;
-       
-        public void PlayTwo(string playerTwo)
+        private void Update()
         {
-            
-            string playerOne = attackChoices[0];
-            
-
-            switch (playerTwo)
+            if (Input.GetKeyDown(KeyCode.RightControl))
             {
-
-                case "HighTwo":
-                    
-                    switch (playerOne)
-                    {
-                        case "High":
-                            
-                            Debug.Log("You also pressed high");
-                            break;
-
-                        case "Mid":
-                            Debug.Log("Check");
-                            break;
-
-                        case "Low":
-                            Debug.Log("Check");
-                            break;
-                    }
-                    Debug.Log("You pressed HIGHTWO");
-                    break;
-
-                case "MidTwo":
-
-                    switch (playerOne)
-                    {
-                        case "High":
-                            Debug.Log("Check");
-                            break;
-
-                        case "Mid":
-                            Debug.Log("Check");
-                            break;
-
-                        case "Low":
-                            Debug.Log("Check");
-                            break;
-                    }
-                    break;
-
-                case "LowTwo":
-
-                    switch (playerOne)
-                    {
-                        case "Up":
-                            Debug.Log("Check");
-
-                            break;
-
-                        case "Mid":
-                            Debug.Log("Check");
-
-                            break;
-
-                        case "Low":
-                            Debug.Log("Check");
-                            break;
-                    }
-                    break;
-
+                UpAttack();
             }
 
+            if(Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                MidAttack();
+            }
+
+            if(Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                DownAttack();
+            }
         }
 
-        public void PlayOne(string playerOne)
+        public void UpAttack()
         {
+            animator.SetTrigger("Up Attack");
+        }
 
-            string playerTwo = playerChoices[0];
+        public void MidAttack()
+        {
+            animator.SetTrigger("Mid Attack");
+        }
 
-
-            switch (playerOne)
-            {
-
-
-                case "High":
-
-                    switch (playerTwo)
-                    {
-                        case "HighTwo":
-
-                            Debug.Log("You also pressed high");
-                            break;
-
-                        case "MidTwo":
-                            Debug.Log("Check");
-                            break;
-
-                        case "LowTwo":
-                            Debug.Log("Check");
-                            break;
-                    }
-                    Debug.Log("friends");
-                    break;
-
-                case "Mid":
-
-                    switch (playerTwo)
-                    {
-                        case "HighTwo":
-                            Debug.Log("Check");
-                            break;
-
-                        case "MidTwo":
-                            Debug.Log("Check");
-                            break;
-
-                        case "LowTwo":
-                            Debug.Log("Check");
-                            break;
-                    }
-                    break;
-
-                case "Low":
-
-                    switch (playerTwo)
-                    {
-                        case "UpTwo":
-                            Debug.Log("Check");
-
-                            break;
-
-                        case "MidTwo":
-                            Debug.Log("Check");
-
-                            break;
-
-                        case "LowTwo":
-                            Debug.Log("Check");
-                            break;
-                    }
-                    break;
-
-            }
-
+        public void DownAttack()
+        {
+            animator.SetTrigger("Down Attack");
         }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,17 +11,24 @@ namespace DanCon
     {
 
         [SerializeField] private float movementSpeed = 5f;
-
+        
+        [SerializeField] private Rigidbody2D samuraiRB;
+        
         [SerializeField] private Animator samuraiAnim;
+        
+        private Vector2 movement;
 
         private void Update()
         {
-            
+            movement.x = Input.GetAxisRaw("Horizontal");
+
+            samuraiAnim.SetFloat("Horizontal", movement.x);
+            samuraiAnim.SetFloat("Speed", movement.sqrMagnitude);
         }
 
         private void FixedUpdate()
         {
-            
+            samuraiRB.MovePosition(samuraiRB.position + movement * movementSpeed * Time.deltaTime);
         }
 
 
