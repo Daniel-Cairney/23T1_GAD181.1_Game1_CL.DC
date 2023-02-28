555b7513b306e10ab369f24e934009e52f3069e3
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2023-02-28 19:30:31 +1100

    Players now die

--- Assets/Scripts/DamageCalculator.cs
+++ Assets/Scripts/DamageCalculator.cs
@@ -14,41 +14,29 @@ namespace DanCon
 
         [SerializeField] private int playerTwoMaxLife = 100;
         [SerializeField] private int playerOneMaxLife = 100;
-        int playerTwoCurrentLife;
-        int playerOneCurrentLife;
 
-        private void Start()
-        {
-            playerTwoCurrentLife = playerTwoMaxLife;
-            playerOneCurrentLife = playerOneMaxLife;
-        }
+
+
         // these must be public so they can be referenced in the attack script
   
         public void HurtPlayerTwo(int damage)
         {
             playerTwoMaxLife -= damage;
-            if(playerTwoCurrentLife <= 0)
+            if(playerTwoMaxLife <= 0)
             {
-                DieTwo();
+                playerTwo.SetActive(false);
+                
             }
         }
-        public void DieTwo()
-        {
-            playerTwo.SetActive(false);
-        }
-
+ 
         public void HurtPlayerOne(int damage)
         {
-            playerTwoMaxLife -= damage;
-            if (playerTwoCurrentLife <= 0)
+            playerOneMaxLife -= damage;
+            if (playerOneMaxLife <= 0)
             {
-                Die();
+                playerOne.SetActive(false); ;
             }
         }
 
-        public void Die()
-        {
-            playerOne.SetActive(false);
-        }
     }
 }
