095a0223dcac73f633d599907ec7f91416e6e481
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2023-02-28 20:19:31 +1100

    player now dies

--- Assets/Scripts/DamageCalculator.cs
+++ Assets/Scripts/DamageCalculator.cs
@@ -15,6 +15,8 @@ namespace DanCon
         [SerializeField] private int playerTwoMaxLife = 100;
         [SerializeField] private int playerOneMaxLife = 100;
 
+        [SerializeField] private GameObject[] pOneSword;
+        [SerializeField] private GameObject[] pTwoSword;
 
 
         // these must be public so they can be referenced in the attack script
@@ -22,6 +24,19 @@ namespace DanCon
         public void HurtPlayerTwo(int damage)
         {
             playerTwoMaxLife -= damage;
+
+            for (int i = 0; i < pTwoSword.Length; i++)
+            {
+                if(playerTwoMaxLife > 1)
+                {
+                    pTwoSword[i].SetActive(true);
+                }
+                else
+                {
+                    pTwoSword[i].SetActive(false);
+                }
+            }
+
             if(playerTwoMaxLife <= 0)
             {
                 playerTwo.SetActive(false);
@@ -32,9 +47,21 @@ namespace DanCon
         public void HurtPlayerOne(int damage)
         {
             playerOneMaxLife -= damage;
+
+            for (int i = 0; i < pOneSword.Length; i++)
+
+                if (playerOneMaxLife > 1)
+                {
+                    pOneSword[i].SetActive(true);
+                }
+                else
+                {
+                    pOneSword[i].SetActive(false);
+                }
+
             if (playerOneMaxLife <= 0)
             {
-                playerOne.SetActive(false); ;
+                playerOne.SetActive(false); 
             }
         }
 
